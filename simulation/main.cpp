cebdf78f85f1438010a40f7b54b9e52358dee3a6
Marcin K <martin.kania@gmail.com>
2019-05-22 00:11:11 +0200

    Iter 0.91
    Refactored simulation code

--- simulation/main.cpp
+++ simulation/main.cpp
@@ -4,10 +4,10 @@
 int main() {
     unsigned seed = 100; //std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(seed);
-    BaseGenerator* g = new GenericGenerator<std::default_random_engine>(generator);
+    BaseGenerator *g = new GenericGenerator<std::default_random_engine>(generator);
     std::cout << NormalDistribution::std_cdf(0) << std::endl;
     std::cout << NormalDistribution::std_rand(g) << std::endl;
-    Distribution* d = new NormalDistribution(0, 1);
+    Distribution *d = new NormalDistribution(0, 1);
     g->seed(seed);
     std::cout << d->cdf(0) << std::endl;
     std::cout << d->rand(g) << std::endl;
--- simulation/sde.h
+++ simulation/sde.h
@@ -7,54 +7,28 @@
 
 // todo: too bad currying doesnt exist here, as an SDESolution is an Generator->Double3Func
 // todo: maybe Ill actually incorporate that observation?!
-typedef std::function<double(BaseGenerator*, double, double)> SDESolution;
-typedef std::function<double(double, double)> Double3Function;
+typedef std::function<double(double, double, double)> SDEFunction;
 
 // todo: maybe template by return type and vector space or smth like that
 class SDE {
-    SDESolution solution;
-    Double3Function quadraticVariationFunc;
-    Double3Function driftFunc;
-    Double3Function diffusionFunc;
+    SDEFunction solutionFunc;
+    SDEFunction quadVarFunc;
+    SDEFunction driftFunc;
+    SDEFunction diffusionFunc;
 public:
-    SDE(SDESolution solution, Double3Function quadraticVariation, Double3Function drift, Double3Function diffusion) :
-        solution(std::move(solution)),
-        quadraticVariationFunc(std::move(quadraticVariation)),
-        driftFunc(std::move(drift)),
-        diffusionFunc(std::move(diffusion))
-    { }
+    SDE(SDEFunction solution, SDEFunction quadraticVariation, SDEFunction drift, SDEFunction diffusion) :
+            solutionFunc(std::move(solution)),
+            quadVarFunc(std::move(quadraticVariation)),
+            driftFunc(std::move(drift)),
+            diffusionFunc(std::move(diffusion)) {}
 
-    double solve(BaseGenerator *g, double x, double t) { return this->solution.operator()(g, x, t); }
-    std::vector<double> solve(BaseGenerator* g, const std::vector<double>& xs, double t) {
-        std::vector<double> result;
-        for(auto x : xs)
-            result.push_back(solve(g, x, t));
-        return result;
-    }
+    double solve(double x, double w, double t) { return this->solutionFunc.operator()(x, w, t); }
 
-    double quadraticVariation(double x, double t) { return this->quadraticVariationFunc.operator()(x, t); }
-    std::vector<double> quadraticVariation(const std::vector<double>& xs, double t) {
-        std::vector<double> result;
-        for(auto x : xs)
-            result.push_back(quadraticVariation(x, t));
-        return result;
-    }
+    double quadVar(double x, double w, double t) { return this->quadVarFunc.operator()(x, w, t); }
 
-    double drift(double x, double t) { return this->driftFunc.operator()(x, t); }
-    std::vector<double> drift(const std::vector<double>& xs, double t) {
-        std::vector<double> result;
-        for(auto x : xs)
-            result.push_back(drift(x, t));
-        return result;
-    }
+    double drift(double x, double w, double t) { return this->driftFunc.operator()(x, w, t); }
 
-    double diffusion(double x, double t) { return this->diffusionFunc.operator()(x, t); }
-    std::vector<double> diffusion(const std::vector<double>& xs, double t) {
-        std::vector<double> result;
-        for(auto x : xs)
-            result.push_back(diffusion(x, t));
-        return result;
-    }
+    double diffusion(double x, double w, double t) { return this->diffusionFunc.operator()(x, w, t); }
 };
 
 // todo: if we had a static initialized map from the SDE type to the function instances, that would be nice
@@ -62,13 +36,13 @@ public:
 class SDEFactory {
 public:
     static SDE makeGBM(double mu, double sigma) {
-        auto solution = [mu, sigma](BaseGenerator* g, double x, double t)->double {
-            return x * std::exp((mu - std::pow(sigma, 2) * 0.5) * t + sigma * NormalDistribution::rand(g, 0.0, t));
+        auto solution = [mu, sigma](double x, double w, double t) -> double {
+            return x * std::exp((mu - std::pow(sigma, 2) * 0.5) * t + sigma * w);
         };
-        auto quadraticVariation = [sigma](double x, double t)->double { return std::pow(sigma * x, 2); };
-        auto drift = [mu](double x, double t)->double { return x*mu; } ;
-        auto diffusion = [sigma](double x, double t)->double { return x*sigma; };
-        SDE sde(solution, quadraticVariation, drift, diffusion);
+        auto quadVar = [sigma](double x, double w, double t) -> double { return std::pow(sigma * x, 2); };
+        auto drift = [mu](double x, double w, double t) -> double { return x * mu; };
+        auto diffusion = [sigma](double x, double w, double t) -> double { return x * sigma; };
+        SDE sde(solution, quadVar, drift, diffusion);
         return sde;
     }
 };
